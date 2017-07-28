09f0ca204c5325d412c3755a1fb13c6197fb472d
Xotic750 <xotic750@gmail.com>
2017-07-28 20:29:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<a href="https://travis-ci.org/Xotic750/is-node-x"
+   title="Travis status">
+<img
+   src="https://travis-ci.org/Xotic750/is-node-x.svg?branch=master"
+   alt="Travis status" height="18"/>
+</a>
+<a href="https://david-dm.org/Xotic750/is-node-x"
+   title="Dependency status">
+<img src="https://david-dm.org/Xotic750/is-node-x.svg"
+   alt="Dependency status" height="18"/>
+</a>
+<a href="https://david-dm.org/Xotic750/is-node-x#info=devDependencies"
+   title="devDependency status">
+<img src="https://david-dm.org/Xotic750/is-node-x/dev-status.svg"
+   alt="devDependency status" height="18"/>
+</a>
+<a href="https://badge.fury.io/js/is-node-x" title="npm version">
+<img src="https://badge.fury.io/js/is-node-x.svg"
+   alt="npm version" height="18"/>
+</a>
+<a name="module_is-node-x"></a>
+
+## is-node-x
+Tests if a value is a DOM Node.
+
+**Version**: 1.0.0  
+**Author**: Xotic750 <Xotic750@gmail.com>  
+**License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
+**Copyright**: Xotic750  
+<a name="exp_module_is-node-x--module.exports"></a>
+
+### `module.exports` ⇒ <code>boolean</code> ⏏
+This method tests if `value` is a DOM Node.
+
+**Kind**: Exported member  
+**Returns**: <code>boolean</code> - True if a DOM Node, otherwise false.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| value | <code>\*</code> | The value to test. |
+
+**Example**  
+```js
+var isNode = require('is-node-x');
+
+isNode(); // => false
+isNode({ nodeType. 1 }); // => false
+isNode(document); // => true
+isNode(document.createNode('div')); // => true
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+/**
+ * @file Tests if a value is a DOM Node.
+ * @version 1.0.0
+ * @author Xotic750 <Xotic750@gmail.com>
+ * @copyright  Xotic750
+ * @license {@link <https://opensource.org/licenses/MIT> MIT}
+ * @module is-node-x
+ */
+
+'use strict';
+
+var documentIsDefined = typeof document !== 'undefined';
+var documentInheritsNode = false;
+var element;
+var hasChildNodes;
+if (documentIsDefined) {
+  try {
+    element = document.createElement('div');
+    hasChildNodes = document.hasChildNodes;
+    documentInheritsNode = typeof hasChildNodes.call(element) === 'boolean';
+  } catch (ignore) {
+    hasChildNodes = null;
+    documentInheritsNode = false;
+  }
+}
+
+if (element && documentInheritsNode === false) {
+  hasChildNodes = element.hasChildNodes;
+}
+
+var $isNode = function isNode(value) {
+  if (hasChildNodes && value && typeof value.nodeType === 'number') {
+    if (value === document) {
+      return true;
+    }
+
+    try {
+      return typeof hasChildNodes.call(value) === 'boolean';
+    } catch (ignore) {}
+
+    if (documentInheritsNode === false) {
+      try {
+        return Boolean(element.cloneNode(false).appendChild(value));
+      } catch (ignore) {}
+    }
+  }
+
+  return false;
+};
+
+/**
+ * This method tests if `value` is a DOM Node.
+ *
+ * @param {*} value The value to test.
+ * @return {boolean} True if a DOM Node, otherwise false.
+ * @example
+ * var isNode = require('is-node-x');
+ *
+ * isNode(); // => false
+ * isNode({ nodeType. 1 }); // => false
+ * isNode(document); // => true
+ * isNode(document.createNode('div')); // => true
+ */
+module.exports = $isNode;
