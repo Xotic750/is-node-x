eeb0ac549dc4bd003a31dd42bad1dad9532c6b81
Graham Fairweather <xotic750@gmail.com>
2019-07-18 01:15:36 +0200

    :bookmark: v2.0.9
    Update dependencies

--- dist/is-node-x.js
+++ dist/is-node-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T18:12:43.057Z",
+  "date": "2019-07-17T23:15:31.148Z",
   "describe": "",
   "description": "Tests if a value is a DOM Node.",
   "file": "is-node-x.js",
   "hash": "ce38523e4b7dfb3849f8",
   "license": "MIT",
-  "version": "2.0.8"
+  "version": "2.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
