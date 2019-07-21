d04550ebcb18d8decd3c2660bd2e8b0506743ea1
Graham Fairweather <xotic750@gmail.com>
2019-07-22 00:10:04 +0200

    :bookmark: v2.0.14
    Auto build

--- dist/is-node-x.js
+++ dist/is-node-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-21T21:43:50.154Z",
+  "date": "2019-07-21T22:09:59.449Z",
   "describe": "",
   "description": "Tests if a value is a DOM Node.",
   "file": "is-node-x.js",
   "hash": "ce38523e4b7dfb3849f8",
   "license": "MIT",
-  "version": "2.0.13"
+  "version": "2.0.14"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
