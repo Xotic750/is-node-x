daddf0c560702dcbd084a960f195c48ddaa1a280
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:13:31 +0200

    :bookmark: v2.0.17
    Add jsdelivr

--- dist/is-node-x.js
+++ dist/is-node-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-23T23:33:32.477Z",
+  "date": "2019-07-24T13:13:26.543Z",
   "describe": "",
   "description": "Tests if a value is a DOM Node.",
   "file": "is-node-x.js",
   "hash": "ce38523e4b7dfb3849f8",
   "license": "MIT",
-  "version": "2.0.16"
+  "version": "2.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
